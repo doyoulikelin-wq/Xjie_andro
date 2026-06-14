9828633aa770d1c221cf38be1aee753e1c6ee914
doyoulikelin-wq <doyoulikelin-wq@users.noreply.github.com>
2026-06-14 21:50:40 +0800

    release: publish android build 2

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         applicationId = "com.xjie.app"
         minSdk = 28
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
